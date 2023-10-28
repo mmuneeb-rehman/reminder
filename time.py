c4202bce451be2781cdf9da246927cf075fd3bdb
mmuneeb-rehman <147839831+mmuneeb-rehman@users.noreply.github.com>
2023-10-28 16:51:29 +0500

    Update time.py

--- time.py
+++ time.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import datetime
+import base64
 
 st.title("Date and Time Calculator")
 
@@ -34,4 +35,14 @@ if save_to_file:
     reminder = f"Reminder: {reminder_message}\nTime: {reminder_time.strftime('%I:%M %p %Z time on %m/%d/%Y')}"
     with open("reminder.txt", "w") as file:
         file.write(reminder)
-    st.write("File Saved Sucessfully")
+    st.write("File Saved Successfully")
+
+# Provide a download link to the user
+
+def get_binary_file_downloader_html(bin_file, file_label):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    b64 = base64.b64encode(data).decode()
+    return f'<a href="data:file/txt;base64,{b64}" download="{file_label}.txt">Download {file_label}</a>'
+if reminder_time is not None:
+    st.markdown(get_binary_file_downloader_html("reminder.txt", "Download Reminder"), unsafe_allow_html=True)
